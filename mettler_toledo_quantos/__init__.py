733a5abfbb16d6cdea17e43f3b913f8285146e80
damarquezg <damarquezg@gmail.com>
2019-11-28 12:13:04 +0000

    Added mettler_toledo_quantos python

---
+++ mettler_toledo_quantos/__init__.py
@@ -0,0 +1,6 @@
+'''
+This Python package (mettler_toledo_quantos) creates a class named 
+MettlerToledoDevice, to interface to Mettler Toledo Quantos that use the Mettler 
+Toledo Quantos Command Set as defined in the reference manual.
+'''
+from .mettler_toledo_quantos import MettlerToledoDevice, MettlerToledoError
---
+++ mettler_toledo_quantos/mettler_toledo_quantos.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+#
+# This is a module for mettler toledo quantos
+#
+# University of Liverpool
+# Autonomous chemistry Lab
+# (C) (2019) David Marquez-Gamez <dmarquez@liverpool.ac.uk>
+
+from __future__ import print_function, division
+import serial
+import time
+import atexit
+import platform
+import os
+try:
+    from exceptions import Exception
+except ImportError:
+    pass
+
+from serial_interface import SerialInterface, SerialInterfaces, find_serial_interface_ports, WriteFrequencyError
+
+DEBUG = True
+BAUDRATE = 9600
+
+class MettlerToledoError(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class MettlerToledoDevice(object):
+    '''
+    This module (mettler_toledo_quantos) creates a class named MettlerToledoDevice, 
+    to interface to Mettler Toledo Quantos using the Mettler Toledo
+    Standard Interface Command Set for Quantos system (MT-SICS-Quantos).
+    '''
+    _TIMEOUT = 0.05
+    _WRITE_WRITE_DELAY = 0.05
+    _RESET_DELAY = 2.0
+
+    def __init__(self,*args,**kwargs):
+        if 'debug' in kwargs:
+            self.debug = kwargs['debug']
+        else:
+            kwargs.update({'debug': DEBUG})
+            self.debug = DEBUG
+        if 'baudrate' not in kwargs:
+            kwargs.update({'baudrate': BAUDRATE})
+        elif (kwargs['baudrate'] is None) or (str(kwargs['baudrate']).lower() == 'default'):
+            kwargs.update({'baudrate': BAUDRATE})
+        if 'timeout' not in kwargs:
+            kwargs.update({'timeout': self._TIMEOUT})
+        if 'write_write_delay' not in kwargs:
+            kwargs.update({'write_write_delay': self._WRITE_WRITE_DELAY})
+        if ('port' not in kwargs) or (kwargs['port'] is None):
+            err_string = 'Specify port.\n'
+            raise RuntimeError(err_string)
+        else:
+            self.port = kwargs['port']
+
+        t_start = time.time()
+        self._serial_device = SerialInterface(*args,**kwargs)
+        atexit.register(self._exit_mettler_toledo_device)
+        time.sleep(self._RESET_DELAY)
+        t_end = time.time()
+        self._debug_print('Initialization time =', (t_end - t_start))
+        
+
+    def _debug_print(self, *args):
+        if self.debug:
+            print(*args)
+
+    def _exit_mettler_toledo_device(self):
+        pass
+
+    def close(self):
+        '''
+        Close the device serial port.
+        '''
+        self._serial_device.close()
+
+    def _args_to_request(self,*args):
+        request = ''.join(map(str,args))
+        request = request + '\r\n';MettlerToledoError
+        return request
+
+    def _send_request_get_response(self,*args):
+    
+        '''Sends request to device over serial port and
+        returns response'''
+
+        request = self._args_to_request(*args)
+        self._debug_print('request', request)
+        response = self._serial_device.write_read(request,use_readline=True,check_write_freq=True)
+        response = response.decode().replace('"','')
+        response_list = response.split()
+        if 'L' in response_list:
+            raise MettlerToledoError('Syntax Error!')
+        return response_list
+
+    def move_frontdoor_open(self):
+        '''
+        Opens the Quantos front door.
+        '''
+        response = self._send_request_get_response('QRA 60 7 3')
+        if 'I' in response[3]:
+            if '1' in response[4]:
+                raise MettlerToledoError('Not mounted.')
+            elif '2'in response[4]:
+                self._debug_print('Another job is running -- retrying.')
+                self.move_frontdoor_open()
+                #raise MettlerToledoError('Another job is running.')
+            elif '3'in response[4]:
+                raise MettlerToledoError('Timeout.')
+            elif '4'in response[4]:
+                raise MettlerToledoError('Not selected.')
+            elif '5'in response[4]:
+                raise MettlerToledoError('Not allowed at the moment')
+            else:
+                raise MettlerToledoError('Stopped by external action.')
+        return response
+
+    def move_frontdoor_close(self):
+        '''
+        Close the Quantos front door.
+        '''
+        #response = self._send_request_get_response('QRA 60 2 4')
+        response = self._send_request_get_response('QRA 60 7 2')
+        if 'I' in response[3]:
+            if '1' in response[4]:
+                raise MettlerToledoError('Not mounted.')
+            elif '2'in response[4]:
+                self._debug_print('Another job is running -- retrying.')
+                self.move_frontdoor_close()
+                #raise MettlerToledoError('Another job is running.')
+            elif '3'in response[4]:
+                raise MettlerToledoError('Timeout.')
+            elif '4'in response[4]:
+                raise MettlerToledoError('Not selected.')
+            elif '5'in response[4]:
+                raise MettlerToledoError('Not allowed at the moment.')
+            else:
+                raise MettlerToledoError('Stopped by external action.')
+        return response
+
+    def quantos_test(self):
+        '''
+        Close the Quantos front door.
+        '''
+        response = self._send_request_get_response('QRA 60 8 0')
+        #response = self._send_request_get_response('QRA 60 2 3')
+        '''
+        if 'I' in response[3]:
+            if '1' in response[4]:
+                raise MettlerToledoError('Not mounted.')
+            elif '2'in response[4]:
+                self._debug_print('Another job is running -- retrying.')
+                self.move_frontdoor_close()
+                #raise MettlerToledoError('Another job is running.')
+            elif '3'in response[4]:
+                raise MettlerToledoError('Timeout.')
+            elif '4'in response[4]:
+                raise MettlerToledoError('Not selected.')
+            elif '5'in response[4]:
+                raise MettlerToledoError('Not allowed at the moment.')
+            else:
+                raise MettlerToledoError('Stopped by external action.')
+        '''
+        return response
+    
+# -----------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    debug = False
+    dev = MettlerToledoDevice(debug=debug)
