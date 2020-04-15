cbc7553505488893f3bb8c13351e77e4b42fa80d
Lewis Jones <lewis.jones@liverpool.ac.uk>
2020-04-15 15:07:09 +0100

    Started Class development

--- GUI_Class.py
+++ GUI_Class.py
@@ -16,3 +16,23 @@ from dicttoxml import dicttoxml
 from xml.dom.minidom import parseString
 import time
 
+
+class QuantosGUI:
+    def __init__(self, master):
+        self.master = master
+
+        # Setting the title of master widget
+        master.title('Quantos Dispensing')
+
+        # Introductory message
+        intro_message = 'Please enter the mass (in mg) to be dispensed below.'
+        self.label = Label(master, text=intro_message, font='helvetica 16 bold', pady=5)
+        self.label.grid(row=0, column=0, columnspan=4)
+
+        # Create table headings
+        self.vial_label = Label(master, text='Vial', font='helvetica 14 bold')
+        self.vial_label.grid(row=1, column=1)
+        self.mass_label = Label(master, text='Mass', font='helvetica 14 bold')
+        self.mass_label.grid(row=1,column=2)
+
+
